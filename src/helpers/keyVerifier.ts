e3ee5f07b0c7e807cfce8a05c14e70f409cbc139
zarkuslol <lucasferlima8@gmail.com>
2022-09-09 14:53:07 -0300

    Troca configuração chave privada

--- src/helpers/keyVerifier.ts
+++ src/helpers/keyVerifier.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export const verifyAccessKey = (req: Request, res: Response, callback: Function, accessKey: string | undefined) => {
-    return (accessKey === "paodealface") ? callback(req, res) : res.status(404).json({ message: "Página não encontrada" });
+    return (accessKey === process.env.PRIVATE_KEY) ? callback(req, res) : res.status(404).json({ message: "Página não encontrada" });
 }
